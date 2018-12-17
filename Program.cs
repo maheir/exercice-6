3532670ab25219a7d04b71f39ef245f9a81e4236
maheir <maheirne@gmail.com>
2018-12-17 17:00:40 +0100

    VARIABLE EXERCICE 6

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b, c;
+            Console.WriteLine("name");
+            a = Console.ReadLine();
+            Console.WriteLine("surname");
+            b = Console.ReadLine();
+            Console.WriteLine("birthday");
+            c = Console.ReadLine();
+            Console.WriteLine(a + "\n"+ b + "\n"+ c);
+
+
+
+
+
+
+        }
+    }
+}
